cb83f7c1c009b75c81653b779afb36eb50f6a0b9
abdurrahman6489 <abdurrahman489@gmail.com>
2023-04-21 23:33:28 +0530

    modifying the code

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 const movieContainer = document.querySelector(".movie-container");
 const inputSearch = document.querySelector(".search");
 let searchedMovie = "";
-let favoriteMovies = [];
-// console.log(movieContainer);
-let movies = [];
+let favoriteMovies = JSON.parse(localStorage.getItem("favoriteMovies")||"[]");
+let favoriteMoviesId = [];
 let newMovies = [];
 async function makeApiCall(sortBy_Index=4,string = ""){
     const response = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=1");
     const data = await response.json();
-    movies.length = 0;
     newMovies.length = 0;
-    movies.push(...data.results);
     newMovies.push(...data.results);
-    console.log(newMovies);
-    console.log("from api call",string);
+    deleteMovieCards();
     if(string!=="") {
         let newArray = newMovies.filter(movie=>movie.title.toLowerCase().includes(string.toLowerCase()));
         console.log(newArray);
@@ -22,22 +18,10 @@ async function makeApiCall(sortBy_Index=4,string = ""){
     }
     else{
         if(sortBy_Index<4){
-            newMovies.sort((a,b)=>{
-                if(sortBy_Index === 0){
-                    return b.vote_average - a.vote_average;
-                }
-                else if(sortBy_Index === 1){
-                    return a.vote_average - b.vote_average;
-                }
-                else if(sortBy_Index === 2){
-                    return b.release_date.localeCompare(a.release_date)  ;
-                }
-                else if(sortBy_Index === 3){
-                    return a.release_date.localeCompare(b.release_date);
-                }
-            })
+            if(sortBy_Index === 0 || sortBy_Index ===1) sortMoviesByRating(sortBy_Index,newMovies);
+            if(sortBy_Index === 2 || sortBy_Index ===3) sortMoviesByRelease(sortBy_Index,newMovies);
         }
-        createMovieCards(newMovies);
+        else createMovieCards(newMovies);
     }   
 }
 function createMovieCards(data){
@@ -84,7 +68,7 @@ function deleteMovieCards(){
         movieContainer.removeChild(movieContainer.firstChild);
     }
 }
-function sortMovies(sortBy_index,array){
+function sortMoviesByRating(sortBy_index,array){
     array.sort((a,b)=>{
         if(sortBy_index === 0){
             return b.vote_average - a.vote_average;
@@ -92,30 +76,30 @@ function sortMovies(sortBy_index,array){
         else if(sortBy_index === 1){
             return a.vote_average - b.vote_average;
         }
-        else if(sortBy_index === 2){
+    })
+    createMovieCards(array);
+}
+function sortMoviesByRelease(sortBy_index,array){
+    array.sort((a,b)=>{
+         if(sortBy_index === 2){
             return b.release_date.localeCompare(a.release_date)  ;
         }
         else if(sortBy_index === 3){
             return a.release_date.localeCompare(b.release_date);
         }
     })
-    return array;
+    createMovieCards(array);
 }
 const sortElem = document.querySelector(".rating-release");
-sortByRatingRelease();
-function sortByRatingRelease(){
+sortByDropDownList();
+function sortByDropDownList(){
     let selectedIndex = 4;
     sortElem.addEventListener("change",(event) => {
         selectedIndex = event.currentTarget.options.selectedIndex;
         console.log(`selected Index is ${selectedIndex}`);
-        deleteMovieCards();
         makeApiCall(selectedIndex);
     })
-    if(selectedIndex>3)
-    {
-    deleteMovieCards();
-    makeApiCall(selectedIndex);
-    }
+    if(selectedIndex>3) makeApiCall(selectedIndex);
 }
 inputSearch.addEventListener("input",(event) => {
     searchedMovie = event.currentTarget.value;
@@ -137,17 +121,18 @@ const updateDebounce = debounce((text)=>{
     makeApiCall(4,text);
 })
 function addFavorite(favoriteBtn){
-    // console.log(favoriteBtn);
+    console.log(favoriteBtn);
     const iconElem = favoriteBtn.querySelector(".fa-bookmark");
     let movieId = favoriteMovies.indexOf(favoriteBtn.id);
     if(movieId===-1){
         favoriteBtn.innerHTML = `<i class="fa-solid fa-bookmark"></i>`;
-        favoriteMovies.push(favoriteBtn.id);
-        // console.log(favoriteMovies);
+        favoriteMovies.push(favoriteBtn.id)
+        console.log(favoriteMovies);
     }
     else{
         favoriteBtn.innerHTML = `<i class="fa-sharp fa-regular fa-bookmark"></i>`;
         favoriteMovies.splice(movieId,1);
-        // console.log(favoriteMovies);
-    } 
+        console.log(favoriteMovies);
+    }
+    localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
 }
